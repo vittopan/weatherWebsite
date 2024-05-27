2656552b9aa56eba314a1ba8e9b8a17e7c668148
vittopan <vittopan19@gmail.com>
2024-05-27 11:04:22 +0800

    random changes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
 from app.forms.forms import LoginForm, createAccount
 from app.models import db, User, Location
 import requests
